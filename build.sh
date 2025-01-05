bb01ea0a67ae0e0a6a32a4d6286c19fe8ea7fb4b
Stefan Breunig <307954+breunigs@users.noreply.github.com>
2025-01-05 10:28:53 +0100

    fix ld warning

--- build.sh
+++ build.sh
@@ -22,12 +22,12 @@ sed -i '/add_dependencies(rife-ncnn-vulkan/,/)/d' "${SUBDIR}/CMakeLists.txt"
 cat << EOF >> "${SUBDIR}/CMakeLists.txt"
   add_custom_command(
       OUTPUT flownetbin.o
-      COMMAND ld -r -b binary flownet.bin -o \${CMAKE_BINARY_DIR}/flownetbin.o -m elf_x86_64
+      COMMAND ld -z noexecstack -r -b binary flownet.bin -o \${CMAKE_BINARY_DIR}/flownetbin.o -m elf_x86_64
       DEPENDS flownet.bin
       WORKING_DIRECTORY \${CMAKE_CURRENT_SOURCE_DIR})
   add_custom_command(
       OUTPUT flownetparam.o
-      COMMAND ld -r -b binary flownet.param -o \${CMAKE_BINARY_DIR}/flownetparam.o -m elf_x86_64
+      COMMAND ld -z noexecstack -r -b binary flownet.param -o \${CMAKE_BINARY_DIR}/flownetparam.o -m elf_x86_64
       DEPENDS flownet.param
       WORKING_DIRECTORY \${CMAKE_CURRENT_SOURCE_DIR})
 
